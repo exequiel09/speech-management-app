3df011a9fc9512f6b176c71143f932bd84bae898
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-03-04 11:57:47 +0800

    Install and setup @datorama/akita

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NG_ENTITY_SERVICE_CONFIG } from '@datorama/akita-ng-entity-service';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { LayoutModule } from './layout';
 import { MainModule } from './modules/main/main.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
@@ -15,12 +18,20 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     BrowserAnimationsModule,
 
+    AkitaNgRouterStoreModule.forRoot(),
     CollapseModule.forRoot(),
 
     LayoutModule,
-    MainModule
+    MainModule,
+
+    ...environment.imports.modules,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: NG_ENTITY_SERVICE_CONFIG,
+      useValue: { baseUrl: environment.apiBaseUrl },
+    },
+  ],
   declarations: [
     AppComponent
   ],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,16 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+
 export const environment = {
-  production: false
+  production: false,
+  apiBaseUrl: ' http://localhost:5000',
+  imports: {
+    modules: [
+      AkitaNgDevtools,
+    ],
+  },
 };
 
 /*
