8582882033c84e152e9fb4adbd2d8f6023aa2120
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-03-04 19:22:35 +0800

    Do not load the entities when it is already loaded

--- src/app/core/state-management/speeches/speeches.query.ts
+++ src/app/core/state-management/speeches/speeches.query.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { QueryEntity } from '@datorama/akita';
 import { RouterQuery } from '@datorama/akita-ng-router-store';
-import { switchMap } from 'rxjs/operators';
+import { distinctUntilChanged, filter, map, switchMap } from 'rxjs/operators';
 
 import { Speech } from '../../models/speech.model';
 import { SpeechesStore, SpeechesState } from './speeches.store';
@@ -11,8 +11,31 @@ import { SpeechesStore, SpeechesState } from './speeches.store';
   providedIn: 'root'
 })
 export class SpeechesQuery extends QueryEntity<SpeechesState, Speech> {
+  readonly isLoaded$ = this.select(['ids', 'loading']).pipe(
+    map(({ ids, loading }: { ids: string[]; loading: boolean; }) => ids.length >= 0 && !loading)
+  );
+
   readonly selectedSpeech$ = this._routerQuery.selectParams('id').pipe(
-    switchMap(id => this.selectEntity(id))
+    switchMap((id: string) => this.selectEntity(id))
+  );
+
+  readonly hasSearchQuery$ = this._routerQuery.selectQueryParams('q').pipe(
+    map((q: string | undefined) => !!q)
+  );
+
+  readonly matchingSpeeches$ = this._routerQuery.selectQueryParams('q').pipe(
+    filter((q: string | undefined) => !!q),
+
+    distinctUntilChanged(),
+
+    switchMap((q: string) => {
+      return this.selectAll({
+        // WARN: if we have a large amount of entities this can be a bottleneck. For now, we'll let it be
+        //       and mitigate it by limiting the number of results returned
+        filterBy: entity => entity.content.toLowerCase().indexOf(q) > -1,
+        limitTo: 50,
+      });
+    })
   );
 
   constructor(
--- src/app/modules/main/pages/view-my-speeches/view-my-speeches.component.ts
+++ src/app/modules/main/pages/view-my-speeches/view-my-speeches.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 
 import { NgEntityServiceLoader } from '@datorama/akita-ng-entity-service';
 import { Subject } from 'rxjs';
-import { delay, map, shareReplay, takeUntil } from 'rxjs/operators';
+import { delay, filter, map, shareReplay, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 import { SpeechesQuery, SpeechesService } from '@speech-management/core/state-management';
 
@@ -28,6 +28,7 @@ export class ViewMySpeechesComponent implements OnDestroy, OnInit {
 
   constructor(
     private readonly _cdr: ChangeDetectorRef,
+    private readonly _ngZone: NgZone,
     private readonly _loader: NgEntityServiceLoader,
     private readonly _speechesQuery: SpeechesQuery,
     private readonly _speechesService: SpeechesService
@@ -39,6 +40,9 @@ export class ViewMySpeechesComponent implements OnDestroy, OnInit {
       )
       .subscribe(loaded => {
         if (loaded) {
+          // Side-effect to trigger the change detection since
+          // `routerLinkActive` directive does not propagate changes to `isActive` property
+          // if the parent component's change detection strategy is set to `OnPush`.
           this._cdr.markForCheck();
         }
       })
@@ -52,8 +56,21 @@ export class ViewMySpeechesComponent implements OnDestroy, OnInit {
   }
 
   ngOnInit() {
-    this._speechesService.get()
+    this._speechesQuery.isLoaded$
       .pipe(
+        tap(() => {
+          // Side-effect to trigger the change detection since
+          // `routerLinkActive` directive does not propagate changes to `isActive` property
+          // if the parent component's change detection strategy is set to `OnPush`.
+          this._ngZone.runOutsideAngular(() => {
+            setTimeout(() => {
+              this._ngZone.run(() => this._cdr.markForCheck());
+            }, 0);
+          })
+        }),
+
+        filter(isLoaded => !isLoaded),
+        switchMap(() => this._speechesService.get()),
         takeUntil(this._unsubscribe$)
       )
       .subscribe()
