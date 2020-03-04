6cede2a95fbe39b8dd4e820338f527f9c15dc622
Exequiel Ceasar Navarrete <exequiel.navarrete09@gmail.com>
2020-03-04 13:43:43 +0800

    Add link to view my speeches component in the navbar

--- src/app/layout/containers/navbar/navbar.component.ts
+++ src/app/layout/containers/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+
+import { Subject } from 'rxjs';
+import { delay, filter, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'sm-navbar',
@@ -6,11 +10,37 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } 
   styleUrls: ['./navbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnDestroy, OnInit {
   @Input() collapsed = true;
   @Input() pageTitle = 'Speech Manager';
 
-  constructor(private _cdr: ChangeDetectorRef) { }
+  private readonly _unsubscribe$ = new Subject<any>();
+
+  constructor(
+    private _cdr: ChangeDetectorRef,
+    private _router: Router
+  ) {
+    // we'll trigger change detection here manually on every NavigationEnd event since
+    // `routerLinkActive` directive does not propagate changes to `isActive` property
+    // if the parent component's change detection strategy is set to `OnPush`.
+    this._router.events
+      .pipe(
+        filter(evt => evt instanceof NavigationEnd),
+
+        delay(0),
+
+        // terminate the pipeline when it receives a notification value from a source observable/subject
+        takeUntil(this._unsubscribe$)
+      )
+      .subscribe(() => this._cdr.markForCheck())
+      ;
+  }
+
+  ngOnDestroy() {
+    // push a notification value to terminate existing subscribers and complete the subject immediately
+    this._unsubscribe$.next();
+    this._unsubscribe$.complete();
+  }
 
   ngOnInit() { }
 
